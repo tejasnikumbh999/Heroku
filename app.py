9c711f8e4f248e05873ce8824868e061c96d6703
tejasnikumbh999 <97099106+tejasnikumbh999@users.noreply.github.com>
2022-02-11 23:46:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import dash
-import dash_auth
+import dash_core_components as dcc
+import dash_html_components as html
 from dash.dependencies import Input, Output, State
-from dash import dcc, html
  
 USERNAME_PASSWORD_PAIRS = [
     ['nethu', '12345'],['guvi', 'guvi']
