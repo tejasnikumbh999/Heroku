0a6cb72b0c1e18be094ae845ea1dcd17fe330679
tejasnikumbh999 <97099106+tejasnikumbh999@users.noreply.github.com>
2022-02-12 00:55:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import plotly.express as px
+import pandas as pd
 import dash
 import dash_auth
 import dash_core_components as dcc
@@ -8,40 +10,52 @@ USERNAME_PASSWORD_PAIRS = [
     ['nethu', '12345'],['guvi', 'guvi']
 ]
  
-app = dash.Dash()
+app = dash.Dash(__name__)
 auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
 server = app.server
+ 
+url="https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
+data=pd.read_csv(url)                                #import external data
+d1=data.groupby("iso_code",as_index=False)['total_cases'].max()
+d1 = d1.dropna(subset=['total_cases'])
+d1
 
-app.layout = html.Div([
-   html.Div('Convert Temperature'),
-   'Celsius',
-   dcc.Input(
-       id="celsius",
-       value=0.0,
-       type="number"
-   ),
-   ' = Fahrenheit',
-   dcc.Input(
-       id="fahrenheit",
-       value=32.0,
-       type="number",
-   ),
+fig = px.scatter_geo(d1, locations="iso_code",
+                    size="total_cases", # size of markers, "total_cases" is one of the columns of covid data
+                    color="iso_code", # which column to use to set the color of markers
+                    hover_name="iso_code", # column added to hover information
+                     )
+fig.update_layout(
+   plot_bgcolor=colors['background'],
+   paper_bgcolor=colors['background'],
+   font_color=colors['text']
+)
+ 
+colors = {
+   'background': '#4FE377',
+   'text': '#111111'
+}
+
+app.layout = html.Div(children=[
+   html.H1(children='Hello ! Welcome to Covid Dashbord',
+           style={
+           'textAlign': 'center',
+           'color': colors['text']
+       }),
+ 
+   html.Div(children='''
+       You can view the total number of cases for a desired country by just moving the pointer at particular country.
+   ''',
+   style={
+           'textAlign': 'center',
+           'color': colors['text']
+       }),
+ 
+   dcc.Graph(
+       id='example-graph',
+       figure=fig
+   )
 ])
  
-@app.callback(
-   Output("celsius", "value"),
-   Output("fahrenheit", "value"),
-   Input("celsius", "value"),
-   Input("fahrenheit", "value"),
-)
-def sync_input(celsius, fahrenheit):
-   ctx = dash.callback_context
-   input_id = ctx.triggered[0]["prop_id"].split(".")[0]
-   if input_id == "celsius":
-       fahrenheit= None if celsius is None else (float(celsius) * 9/5) + 32
-   else:
-       celsius = None if fahrenheit is None else (float(fahrenheit) - 32) * 5/9
-   return celsius, fahrenheit
- 
-if __name__ == "__main__":
+if __name__ == '__main__':
    app.run_server(debug=True)
