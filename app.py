13edcc39b1e98e9f605bbe68ddad2cae5a52e01f
tejasnikumbh999 <97099106+tejasnikumbh999@users.noreply.github.com>
2022-02-11 23:39:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import dash
 import dash_auth
-import dash_core_components as dcc
-import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
+from dash import dcc, html
  
 USERNAME_PASSWORD_PAIRS = [
     ['nethu', '12345'],['guvi', 'guvi']
@@ -12,19 +11,40 @@ app = dash.Dash()
 auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
 server = app.server
 
+import dash
+from dash.dependencies import Input, Output, State
+from dash import dcc, html
+
 app.layout = html.Div([
-   html.Button("Download Text", id="btn-download"),
-   dcc.Download(id="download-text")
+   html.Div('Convert Temperature'),
+   'Celsius',
+   dcc.Input(
+       id="celsius",
+       value=0.0,
+       type="number"
+   ),
+   ' = Fahrenheit',
+   dcc.Input(
+       id="fahrenheit",
+       value=32.0,
+       type="number",
+   ),
 ])
  
 @app.callback(
-   Output("download-text", "data"),
-   Input("btn-download", "n_clicks"),
-   prevent_initial_call=True,
+   Output("celsius", "value"),
+   Output("fahrenheit", "value"),
+   Input("celsius", "value"),
+   Input("fahrenheit", "value"),
 )
-def func(n_clicks):
-   return dict(content="Hello world!", filename="hello.txt")
- 
+def sync_input(celsius, fahrenheit):
+   ctx = dash.callback_context
+   input_id = ctx.triggered[0]["prop_id"].split(".")[0]
+   if input_id == "celsius":
+       fahrenheit= None if celsius is None else (float(celsius) * 9/5) + 32
+   else:
+       celsius = None if fahrenheit is None else (float(fahrenheit) - 32) * 5/9
+   return celsius, fahrenheit
  
 if __name__ == "__main__":
    app.run_server(debug=True)
