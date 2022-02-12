15cc4bfc40b55943321e60eac813bce372830090
tejasnikumbh999 <97099106+tejasnikumbh999@users.noreply.github.com>
2022-02-12 10:09:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,52 +10,40 @@ USERNAME_PASSWORD_PAIRS = [
     ['nethu', '12345'],['guvi', 'guvi']
 ]
  
-app = dash.Dash(__name__)
+app = dash.Dash(
 auth = dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
 server = app.server
- 
-url="https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv"
-data=pd.read_csv(url)                               
-d1=data.groupby("iso_code",as_index=False)['total_cases'].max()
-d1 = d1.dropna(subset=['total_cases'])
-d1
 
-fig = px.scatter_geo(d1, locations="iso_code",
-                    size="total_cases", 
-                    color="iso_code", 
-                    hover_name="iso_code",
-                     )
-fig.update_layout(
-   plot_bgcolor=colors['background'],
-   paper_bgcolor=colors['background'],
-   font_color=colors['text']
-)
- 
-colors = {
-   'background': '#4FE377',
-   'text': '#111111'
-}
-
-app.layout = html.Div(children=[
-   html.H1(children='Hello ! Welcome to Covid Dashbord',
-           style={
-           'textAlign': 'center',
-           'color': colors['text']
-       }),
- 
-   html.Div(children='''
-       You can view the total number of cases for a desired country by just moving the pointer at particular country.
-   ''',
-   style={
-           'textAlign': 'center',
-           'color': colors['text']
-       }),
- 
-   dcc.Graph(
-       id='example-graph',
-       figure=fig
-   )
+app.layout = html.Div([
+   html.Div('Convert Temperature'),
+   'Celsius',
+   dcc.Input(
+       id="celsius",
+       value=0.0,
+       type="number"
+   ),
+   ' = Fahrenheit',
+   dcc.Input(
+       id="fahrenheit",
+       value=32.0,
+       type="number",
+   ),
 ])
  
-if __name__ == '__main__':
+@app.callback(
+   Output("celsius", "value"),
+   Output("fahrenheit", "value"),
+   Input("celsius", "value"),
+   Input("fahrenheit", "value"),
+)
+def sync_input(celsius, fahrenheit):
+   ctx = dash.callback_context
+   input_id = ctx.triggered[0]["prop_id"].split(".")[0]
+   if input_id == "celsius":
+       fahrenheit= None if celsius is None else (float(celsius) * 9/5) + 32
+   else:
+       celsius = None if fahrenheit is None else (float(fahrenheit) - 32) * 5/9
+   return celsius, fahrenheit
+ 
+if __name__ == "__main__":
    app.run_server(debug=True)
